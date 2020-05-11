4d79de979c044c9d89013e9c86a9dfff72a2e0b5
Bhaskar <bhaskar2choudhary@gmail.com>
2020-05-12 02:35:32 +0530

    added google analytics tag

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag"; // Google Analytics
+
+Vue.use(
+  VueGtag,
+  {
+    config: { id: "UA-163755956-1" },
+  },
+  router
+);
 
 import { store } from "./store/store";
 
